471d14eee3b0abfeb5d64bbe08aed5e2bee27b48
BaptisteGarcin <baptiste.garcin@serli.com>
2017-06-02 10:31:44 +0200

    keyboard keys listeners

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const sphero = require("sphero");
+const readline = require('readline');
+
 
 orb_chris = sphero("COM6");
 orb_serli = sphero("COM4");
@@ -17,3 +19,19 @@ orb_serli.connect(function () {
     orb_serli.color('red');
 });
 
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', (str, key) => {
+    if (key.ctrl && key.name === 'c') {
+    process.exit();
+} else {
+    console.log(`You pressed the "${str}" key`);
+    if(str === '1'){
+        console.log('orb serli')
+    }
+    else if(str === '2'){
+        console.log('orb chris')
+    }
+}
+});
+console.log('Press any key...');
