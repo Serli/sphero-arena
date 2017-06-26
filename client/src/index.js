e081c360a2a7da0eb23e73618b1496651d9447f8
BaptisteGarcin <baptiste.garcin@serli.com>
2017-06-26 17:32:58 +0200

    shoot button responsive

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Arrows from "./Arrows";
@@ -13,7 +12,6 @@ let socket = io('http://192.168.86.134:8080');
 
 let props = {socket:socket};
 
-ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(React.createElement(Arrows, props), document.getElementById('arrows'));
 ReactDOM.render(React.createElement(OrbsConnect, props), document.getElementById('orbsConnect'));
 ReactDOM.render(React.createElement(FloorPlan, props), document.getElementById('floorPlan'));
