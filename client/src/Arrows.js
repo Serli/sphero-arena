49eda9ea0c0e6785a4e820328834068bef19779a
BaptisteGarcin <baptiste.garcin@serli.com>
2017-06-27 14:25:14 +0200

    orb manipulation on holding buttons

--- client/src/Arrows.js
+++ client/src/Arrows.js
@@ -4,49 +4,45 @@ import React, { Component } from 'react';
 class Arrows extends Component {
     constructor(props){
         super(props);
-        this.state = {value: ''};
         // This binding is necessary to make `this` work in the callback
         this.handleClickUp = this.handleClickUp.bind(this);
         this.handleClickLeft = this.handleClickLeft.bind(this);
-        this.handleClickDown = this.handleClickDown.bind(this);
         this.handleClickRight = this.handleClickRight.bind(this);
         this.handleChange = this.handleChange.bind(this);
-        this.var;
     }
 
     handleClickUp() {
-        this.props.socket.emit('go forward');
+        let socket = this.props.socket;
+        this.up = setInterval(function () {
+            socket.emit('go forward');
+        }, 500);
     }
 
     handleClickLeft() {
-        this.props.socket.emit('left');
-    }
-
-    handleClickDown() {
-        this.props.socket.emit('stop');
+        let socket = this.props.socket;
+        this.left = setInterval(function () {
+            socket.emit('left');
+        }, 500);
     }
 
     handleClickRight() {
         let socket = this.props.socket;
         this.var = setInterval(function () {
             socket.emit('right');
-            console.log('TEST');
         }, 500);
     }
 
     handleChange() {
         clearInterval(this.var);
+        clearInterval(this.left);
+        clearInterval(this.up);
     }
 
     render() {
         return(
-
             <div className="arrow-key-container">
-                <div className="arrow-key up" data-key="38" onClick={this.handleClickUp}></div><br/>
-                <div className="arrow-key left" data-key="37" onClick={this.handleClickLeft}></div>
-                <div className="arrow-key down" data-key="40" onClick={this.handleClickDown}>
-                    <i className="glyphicon glyphicon-pause"></i>
-                </div>
+                <div className="arrow-key up" data-key="38" onMouseDown={this.handleClickUp} onMouseUp={this.handleChange}></div><br/>
+                <div className="arrow-key left" data-key="37" onMouseDown={this.handleClickLeft} onMouseUp={this.handleChange}></div>
                 <div className="arrow-key right" data-key="39" onMouseDown={this.handleClickRight} onMouseUp={this.handleChange}></div>
             </div>
         )
